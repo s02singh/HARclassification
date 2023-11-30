33ca728f6f8e91bbcf0abb37675e527933e13730
s02singh <sahilbirsingh221@gmail.com>
2023-11-29 22:19:24 -0800

    updated model

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ import joblib
 app = Flask(__name__)
 
 # Load the pre-trained model and scaler
-model = joblib.load('logistic_regression_model.pkl')
+model = joblib.load('random_forest_model.pkl')
 scaler = joblib.load('standard_scaler.pkl')
+
+# Define the selected features
+selected_features = ['Sex', 'Diabetes', 'Family History', 'Smoking', 'Obesity', 'Alcohol Consumption', 'Diet',
+                     'Previous Heart Problems', 'Exercise Hours Per Week', 'Sleep Hours Per Day', 'Cholesterol']
 
 @app.route('/')
 def home():
@@ -16,24 +20,41 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get input values from the form
-    sleep_hours = float(request.form['sleep_hours'])
-    cholesterol = float(request.form['cholesterol'])
-    obesity = int(request.form['obesity'])
+    sex = int(request.form['sex'])
     diabetes = int(request.form['diabetes'])
+    family_history = int(request.form['family_history'])
+    smoking = int(request.form['smoking'])
+    obesity = int(request.form['obesity'])
+    alcohol_consumption = int(request.form['alcohol_consumption'])
+    diet = int(request.form['diet'])
+    previous_heart_problems = int(request.form['previous_heart_problems'])
+    exercise_hours_per_week = float(request.form['exercise_hours_per_week'])
+    sleep_hours_per_day = float(request.form['sleep_hours_per_day'])
+    cholesterol = float(request.form['cholesterol'])
+    
 
     # Create a DataFrame with the input values
-    input_data = pd.DataFrame({'Sleep Hours Per Day': [sleep_hours],
-                               'Cholesterol': [cholesterol],
-                               'Obesity': [obesity],
-                               'Diabetes': [diabetes]})
+    input_data = pd.DataFrame({
+        'Sex': [sex],
+        'Diabetes': [diabetes],
+        'Family History': [family_history],
+        'Smoking': [smoking],
+        'Obesity': [obesity],
+        'Alcohol Consumption': [alcohol_consumption],
+        'Diet': [diet],
+        'Previous Heart Problems': [previous_heart_problems],
+        'Exercise Hours Per Week': [exercise_hours_per_week],
+        'Sleep Hours Per Day': [sleep_hours_per_day],
+        'Cholesterol': [cholesterol],
+    })
 
-    # Scale and normalize the input data
-    input_data_scaled = scaler.transform(input_data)
 
-    # Make a prediction
-    prediction = model.predict(input_data_scaled)[0]
+
+    # make a prediction
+    input_data = input_data[selected_features]
+    prediction = model.predict(input_data)[0]
 
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(port=8080,debug=True)
+    app.run(port=8080, debug=True)
